3bf4219de642d4853003723941dcfebf3d3b4166
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-03-16 14:21:12 +0400

    Everything is compilable now omfg

--- session-ui/SwiftExtensions/QRCodes.swift
+++ session-ui/SwiftExtensions/QRCodes.swift
@@ -6,17 +6,17 @@
 //
 
 import Foundation
-import EFQRCode
+//import EFQRCode
 import SwiftUI
 
-func generateQRCode(from string: String) -> UIImage? {
-  if let image = EFQRCode.generate(
-      for: string,
-      backgroundColor: UIColor(Color.background).cgColor,
-      foregroundColor: UIColor(Color.brand).cgColor
-  ) {
-      return UIImage(cgImage: image)
-  } else {
-      return nil
-  }
-}
+//func generateQRCode(from string: String) -> UIImage? {
+//  if let image = EFQRCode.generate(
+//      for: string,
+//      backgroundColor: UIColor(Color.background).cgColor,
+//      foregroundColor: UIColor(Color.brand).cgColor
+//  ) {
+//      return UIImage(cgImage: image)
+//  } else {
+//      return nil
+//  }
+//}
